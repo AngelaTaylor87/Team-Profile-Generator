2a44688def1715cb5ac9fcc83ef464048020807e
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-07-30 11:21:50 -0500

    updated classes and html

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 const { choices } = require('yargs');
+const teamArray = []
+
+
+
 inquirer 
   .prompt([ {
     type: 'input',
     name: 'name',
-    message: 'What is the name of the manager?'
+    message: 'What is the team manager name?'
   },
   {
       type: 'input',
       name: 'id',
-      message: 'What is the managers id?'
+      message: 'What is the team manager id?'
   },
   {
     type: 'input',
     name: 'email',
-    message: 'What is the managers email?'
+    message: 'What is the manager email?'
   },
   {
     type: 'input',
     name: 'officeNumber',
-    message: 'What is the managers office number?'
+    message: 'What is the manager office number?'
+  },
+  {
+      type: 'list',
+      name: 'role',
+      message: 'Select a team member role you would you like to add.',
+      choices: [
+          'Engineer',
+          'Intern'
+      ]
+
+
   } 
   ])
+  
+  
+
   .then((answers) => {
       console.log(`${answers.name} works in office ${answers.officeNumber}`);
     // Use user feedback for... whatever!!
   })
-  
- 
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
@@ -35,3 +56,4 @@ inquirer
       // Something else went wrong
     }
   });
+  
